fc26a59f06a874094086dfb87d1a74c4159c022a
kogicko240 <dmn120112@gmail.com>
2026-03-02 17:53:19 +0300

    17:53

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -9,5 +9,21 @@
 #include "logic.h"
 
 void swap_extrem_elements(int array[DEFAULT_SIZE], int size) {
+	if (size < 2) return;
 
+	int minVal = array[0];
+	int maxVal = array[0];
+	int minIdx = 0;
+	int maxIdx = 0;
+
+	for (int i = 1; i < size; i++) {
+		if (array[i] < minVal) {
+			minVal = array[i];
+			maxIdx = i;
+		}
+	}
+
+	int temp = array[minIdx];
+	array[minIdx] = array[maxIdx];
+	array[maxIdx] = temp;
 }
