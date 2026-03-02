f9b3fa6d3d3a1e89e382791ba0e87a088ec3c75c
kogicko240 <dmn120112@gmail.com>
2026-03-02 17:46:25 +0300

    17:46

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -7,11 +7,14 @@
 
 #include "logic.h"
 bool is_the_same_elements(int array[DEFAULT_SIZE], int size) {
-	for (int i = 0; i < size; i++) {
+	if (size <= 0 || size < 2) return false;
+
+	for (int i = 0; i < size - 1; i++) {
 		for (int j = i + 1; j < size; j++) {
-			if (array[DEFAULT_SIZE] == array[DEFAULT_SIZE]) {
+			if (array[i] == array[j]) {
 				return true;
 			}
 		}
 	}
+	return false;
 }
